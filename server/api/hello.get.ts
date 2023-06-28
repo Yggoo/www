a9bd33eb647bc939fe7092f3f697681d2950417b
Johannes Skov Frandsen <jsf@greenoak.dk>
2023-06-28 13:26:33 +0000

    tweak

--- server/api/hello.get.ts
+++ server/api/hello.get.ts
@@ -2,11 +2,13 @@ import { createApi } from 'unsplash-js'
 import { Random } from 'unsplash-js/dist/methods/photos/types'
 
 export default defineEventHandler(async () => {
+  const { UNSPLASH_ACCESS_KEY} = useRuntimeConfig()
   const serverApi = createApi({
-    accessKey: useRuntimeConfig().UNSPLASH_ACCESS_KEY
+    accessKey: UNSPLASH_ACCESS_KEY
   })
   const output = await serverApi.photos.getRandom({})
-  // return (output.response as Random).urls.small
-  const config = useRuntimeConfig()
-  return { key: config.NO_DANGER }
+  return { 
+    url: (output.response as Random).urls.small,
+    raw: output
+  }
 })
